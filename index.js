70469be0200b82d54a37988d50c093d10c2f898e
Dmitry Ovsyanko <dovsyanko@cheby.ru>
2026-08-27 15:05:18 +0300

    Application/* -constructor

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
-const conf   = require ('./lib/app/Conf')
+const cluster = require ('node:cluster')
+const kind    = cluster.isPrimary ? 'Primary' : 'Worker'
+const conf    = require ('./lib/app/Conf')
+const logger  = require ('./lib/app/Logger') (conf)
+const app     = new (require (`./lib/app/Application/${kind}`)) (conf, logger)
 
-const logger = require ('./lib/app/Logger') (conf)
-
-const app    = new (require (`./lib/app/Application/${
-
-    require ('node:cluster').isPrimary ? 'Primary' : 'Worker'
-
-}`)) (conf, logger)
+app.spawn ({
+    type: kind.toLowerCase () + '_process',
+    action: 'create',
+})
